3cb1eca81d352a1d38b0733a9cf06ed8a83e9f64
Giayychan <giayanny926@gmail.com>
2020-03-02 22:49:50 +0800

    added login auth in client

--- src/Login.js
+++ src/Login.js
@@ -1,24 +1,59 @@
 import React from 'react'
+import axios from 'axios'
+import { Redirect, Route } from 'react-router-dom'
+require('dotenv').config()
 
 class Login extends React.Component {
 	// Data
-	state = {}
+	state = { email: '', password: '', redirect: false }
 	// Methods
-	login = () => {}
+	login = e => {
+		e.preventDefault()
+		if (this.state.email === '' || this.state.password === '') {
+			this.setState({ error: 'Please put Email and Password!' })
+		} else {
+			axios
+				.post(`${process.env.REACT_APP_API}/users/login`, this.state)
+				.then(data => {
+					if (data.data.error !== undefined) {
+						this.setState(data.data)
+					} else {
+						localStorage.setItem('key', data.data)
+						this.setState({ redirect: true })
+					}
+				})
+		}
+	}
+
+	add = e => {
+		if (e.target.placeholder === 'Email') {
+			this.setState({ email: e.target.value })
+		}
+		if (e.target.placeholder === 'Password') {
+			this.setState({ password: e.target.value })
+		}
+	}
+
 	// Render
 	render() {
 		return (
-			<form className="card" onSubmit={this.login}>
-				<input type="text" placeholder="Email" />
-				<input type="password" placeholder="Password" />
-				<button type="submit" className="positive">
-					Login
-				</button>
-				<div className="link">
-					<a href="/signup">New here? Create an account</a>
-				</div>
-				<div className="error">{this.state.error}</div>
-			</form>
+			<>
+				<Route
+					path="/"
+					render={() => (this.state.redirect ? <Redirect to="/" /> : null)}
+				/>
+				<form className="card" onSubmit={this.login}>
+					<input type="text" placeholder="Email" onChange={this.add} />
+					<input type="password" placeholder="Password" onChange={this.add} />
+					<button type="submit" className="positive">
+						Login
+					</button>
+					<div className="link">
+						<a href="/signup">New here? Create an account</a>
+					</div>
+					<div className="error">{this.state.error}</div>
+				</form>
+			</>
 		)
 	}
 }
