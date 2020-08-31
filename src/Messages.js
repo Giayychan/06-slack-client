1db649357d3430f4fcaae8c7ef2c093d44a72153
Giayychan <giayanny926@gmail.com>
2020-08-31 10:50:17 +1000

    fixed bugs

--- src/Messages.js
+++ src/Messages.js
@@ -1,81 +1,89 @@
-import React, { Component } from 'react'
-import './styles/Messages.css'
-import './styles/NewMessage.css'
-import axios from 'axios'
-import moment from 'moment'
+import React, { Component } from 'react';
+import './styles/Messages.css';
+import './styles/NewMessage.css';
+import axios from 'axios';
+import moment from 'moment';
 
 class Content extends Component {
+	constructor(props) {
+		super(props);
+		this.messagesEndRef = React.createRef();
+	}
 	// Data
 	state = {
 		newMessage: {
 			channel: '',
 			text: '',
-			file: null,
+			file: null
 		},
 		messages: [],
-		selectedId: '',
-	}
+		selectedId: ''
+	};
 	// Lifecycle
 	componentWillReceiveProps = (props) => {
-		if (props.selected)
-			axios
-				.get(
-					`${process.env.REACT_APP_API}/messages?channel=${props.selected}`,
-					{
-						headers: {
-							Authorization: `Bearer ${localStorage.getItem('token')}`,
-						},
-					}
-				)
-				.then((res) => {
-					this.setState({ messages: res.data, selectedId: props.selected })
-					console.log('axios', res.data)
-				})
-	}
+		axios
+			.get(`${process.env.REACT_APP_API}/messages?channel=${props.selected}`, {
+				headers: {
+					Authorization: `Bearer ${localStorage.getItem('token')}`
+				}
+			})
+			.then((res) => {
+				this.setState({ messages: res.data, selectedId: props.selected });
+				//console.log('axios', res.data);
+				this.scrollToBottom();
+			});
+	};
+
 	// Methods
+	scrollToBottom = () => {
+		this.messagesEndRef.current.scrollIntoView({ behavior: 'smooth' });
+	};
+
 	changeText = (e) => {
-		let newMessage = this.state.newMessage
-		newMessage.text = e.target.value
-		newMessage.channel = this.state.selectedId
-		this.setState({ newMessage })
-	}
+		let newMessage = this.state.newMessage;
+		newMessage.text = e.target.value;
+		newMessage.channel = this.state.selectedId;
+		this.setState({ newMessage });
+	};
 	createMessage = (e) => {
-		e.preventDefault()
-		let formData
+		e.preventDefault();
+		let formData;
 		if (this.state.newMessage.file) {
-			formData = new FormData()
-			formData.append('file', this.state.newMessage.file)
-			formData.append('channel', this.state.selectedId)
-			formData.append('text', this.state.newMessage.text)
+			formData = new FormData();
+			formData.append('file', this.state.newMessage.file);
+			formData.append('channel', this.state.selectedId);
+			formData.append('text', this.state.newMessage.text);
 		} else {
-			formData = this.state.newMessage
+			formData = this.state.newMessage;
 		}
 
 		axios
 			.post(`${process.env.REACT_APP_API}/messages`, formData, {
 				headers: {
-					Authorization: `Bearer ${localStorage.getItem('token')}`,
-				},
+					Authorization: `Bearer ${localStorage.getItem('token')}`
+				}
 			})
 			.then((res) => {
-				let messages = this.state.messages
-				messages.push(res.data)
-				this.setState({ messages })
-				console.log('axiosgetmessages', this.state.messages)
-				let newMessage = this.state.newMessage
-				newMessage.text = ''
-				newMessage.file = null
-				this.setState({ newMessage })
-				console.log('axiosgetmessages', this.state.newMessage)
+				let messages = this.state.messages;
+				//	console.log(res.data)
+				messages.push(res.data);
+				this.setState({ messages });
+				//	console.log('axiosgetmessages', this.state.messages)
+				let newMessage = this.state.newMessage;
+				newMessage.text = '';
+				newMessage.file = null;
+				this.setState({ newMessage });
+				//	console.log('axiosgetmessages', this.state.newMessage)
+				this.scrollToBottom();
 			})
-			.catch((err) => console.log(err))
-	}
+			.catch((err) => console.log(err));
+	};
 
 	addFile = (e) => {
-		let newMessage = this.state.newMessage
-		newMessage.file = e.target.files[0]
-		this.setState({ newMessage })
-	}
+		let newMessage = this.state.newMessage;
+		newMessage.file = e.target.files[0];
+		this.setState({ newMessage });
+	};
 
 	// Render
 	render() {
@@ -90,15 +98,16 @@ class Content extends Component {
 									{moment(message.date).format('DD MMMM YYYY HH:mm')}
 								</span>
 								<div className='body'>{message.text}</div>
-								<img src={message.file}></img>
+								{message.file && <img src={message.file} alt='message'></img>}
 							</div>
-						)
+						);
 					})}
+					<div ref={this.messagesEndRef} />
 				</div>
 				<div id='new-message'>
 					<form
 						onSubmit={(e) => {
-							this.createMessage(e)
+							this.createMessage(e);
 						}}
 					>
 						<input type='file' name='file' onChange={this.addFile} />
@@ -114,8 +123,8 @@ class Content extends Component {
 					</form>
 				</div>
 			</div>
-		)
+		);
 	}
 }
 
-export default Content
+export default Content;
